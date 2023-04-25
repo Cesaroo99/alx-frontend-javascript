1e9ab00c37f28e72c124dcf6876ff9ca608df019
Memoli Nomo Cesar Auguste <memolicesar1@gmail.com>
2023-04-25 21:51:37 +0000

    tasks

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const i = new Map();
+  i.set('Apples', 10);
+  i.set('Tomatoes', 10);
+  i.set('Pasta', 1);
+  i.set('Rice', 1);
+  i.set('Banana', 5);
+
+  return i;
+}
